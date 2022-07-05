6102511b18a4c24b25ffc6fdf22b756072ec92dd
PicoPicoRobotWoman <105271825+PicoPicoRobotWoman@users.noreply.github.com>
2022-07-05 13:00:34 +0300

    г

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -7,8 +7,8 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     // Метод, указывающий на класс конфигурации
     @Override
     protected Class<?>[] getRootConfigClasses() {
-       return null;
-       //return new Class[] { HibernateConfig.class };
+       //return null;
+       return new Class[] { HibernateConfig.class };
     }
 
 
